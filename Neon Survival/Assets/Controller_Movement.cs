8c28bf1f3906ae1591735dfedd1f7475bc64b9a8
samuelhegner <C16439262@mydit.ie>
2019-02-23 14:59:19 +0000

    First Push

---
+++ Neon Survival/Assets/Controller_Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Movement : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed;
+
+    [Range(0, 10)]
+    public float speedLossPercentagePerSecond;
+
+    [Range(1, 2)]
+    public int playerNum;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float hAxis;
+        float yAxis;
+
+        if (playerNum == 1)
+        {
+            hAxis = Input.GetAxis("Horizontal_P1");
+            yAxis = Input.GetAxis("Vertical_P1");
+        }
+        else {
+            hAxis = Input.GetAxis("Horizontal_P2");
+            yAxis = Input.GetAxis("Vertical_P2");
+        }
+        
+
+        rb.AddForce(new Vector3(hAxis, 0, yAxis) * speed, ForceMode.VelocityChange);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+
+        rb.velocity *= (1.0f - (speedLossPercentagePerSecond * Time.deltaTime));
+
+        if (rb.velocity != Vector3.zero)
+        transform.rotation = Quaternion.LookRotation(rb.velocity, transform.up);
+    }
+
+    public void SwtichPlayer() {
+        if (playerNum == 1)
+        {
+            playerNum = 2;
+        }
+        else if (playerNum == 2) {
+            playerNum = 1;
+        }
+        
+    }
+}
+
---
+++ Neon Survival/Assets/Switch_Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Player : MonoBehaviour
+{
+    int playerNum;
+
+    void Start()
+    {
+        playerNum = GetComponent<Controller_Movement>().playerNum;
+        print(playerNum);
+    }
+
+    void Update()
+    {
+        if (playerNum == 1)
+        {
+            if (Input.GetButtonDown("X_P1"))
+            {
+                print("X Pressed");
+            }
+        }
+        else if (playerNum == 2) {
+            if (Input.GetButtonDown("X_P2")) {
+                print("X Pressed");
+            }
+        }
+    }
+}
