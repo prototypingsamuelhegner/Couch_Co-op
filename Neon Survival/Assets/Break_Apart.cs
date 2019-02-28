4d563ddaf97103ed7846ef3b6f59193cb2c66b4d
samuelhegner <C16439262@mydit.ie>
2019-02-28 16:46:32 +0000

    Breaking Start

---
+++ Neon Survival/Assets/Break_Apart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break_Apart : MonoBehaviour
+{
+    Destroy_Self destroy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Break(){
+        
+    }
+}
