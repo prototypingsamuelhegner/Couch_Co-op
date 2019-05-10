9cbd6ce2eba41123965ee95bede4e007083407f2
CC172 <43939055+CC172@users.noreply.github.com>
2019-05-10 11:51:35 +0100

    Cinematic added

---
+++ Neon Survival/Assets/Scripts 1/Countdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountdownTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+    IEnumerator CountdownTimer()
+    {
+        yield return new WaitForSeconds(18);
+        SceneManager.LoadScene(2);
+    }
+}
